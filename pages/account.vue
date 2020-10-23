df770d912236b2b7160046effacb685e292fe045
Polaf05 <paularpolo1@gmail.com>
2020-10-23 09:46:50 +0800

    ForgotPassword,RememberMe,Modal,CheckBox

---
+++ pages/account.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form @submit.prevent="signout">
+        <button>Log out!</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import * as firebase from "firebase/app";
+import "firebase/auth";
+export default {
+    methods:{
+        signout(){
+            firebase.auth().signOut().then(user =>{
+                    console.log(user);
+                    this.$router.push("/");
+                }).catch(function(error) {
+                    console.log(error);
+                });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
